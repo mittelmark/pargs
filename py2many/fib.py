73d242a843bfaa8c606dd0d76a46a5e43eef100f
Detlef Groth <dgroth@d7480>
2026-01-12 22:33:04 +0000

    support for Julia and Rust added

---
+++ py2many/fib.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import sys
+class Hi ():
+    def hello(self):
+        print("hello")
+    
+def fib(i: int) -> int:
+    if i == 0 or i == 1:
+        return 1
+    return fib(i - 1) + fib(i - 2)
+
+# Demonstrate overflow handling
+def add(i: int, j: int) -> int:
+    return i + j
+
+if __name__ == "__main__":
+    print(fib(12))
+    print(add(1,12))
+    h = Hi()
+    h.hello()
---
+++ py2many/out/run.nim
@@ -0,0 +1,22 @@
+#[
+ # ###########################################################
+ #  Copyright (c) 2026, MicroEmacs User
+ #  License       :                     
+ #  Date          : $Date$              
+ #  Author        : $Author$            
+ #  Created By    : MicroEmacs User         
+ #  Created       : 2026-01-12 21:33:20        
+ #  Last Modified : <260112.2142>       
+ #                                      
+ #  Description   :                     
+ # ###########################################################                                     
+]# 
+
+include fib
+
+when isMainModule:
+  echo fib(12)
+  echo add(1, 12)
+  let h = Hi()
+  h.hello()
+
