7865df52cf71dcaf0a36c9790a10bacc9f348572
Detlef Groth <dgroth@d7480>
2026-01-13 01:19:02 +0000

    adding dart support

--- py2many/fib.py
+++ py2many/fib.py
@@ -3,7 +3,11 @@ import sys
 class Hi ():
     def hello(self):
         print("hello")
-    
+        
+    def length(self, lst: List[str]) -> int:
+        return int(len(lst))
+        
+        
 def fib(i: int) -> int:
     if i == 0 or i == 1:
         return 1
@@ -18,3 +22,5 @@ if __name__ == "__main__":
     print(add(1,12))
     h = Hi()
     h.hello()
+    v = ["Hello", "World","Dummy"]
+    print(h.length(v))
