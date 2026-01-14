9e1b5f863e5473c3fb7cb332760006b7a65c9d72
Detlef Groth <dgroth@d7480>
2026-01-14 10:36:02 +0000

    adding typsescript support

---
+++ fusion/ts/run-pargs.js
@@ -0,0 +1,15 @@
+/* -*- javascript -*- 
+ * Copyright (C) 2026 MicroEmacs User.
+ *
+ * All rights reserved.
+ *
+ * Synopsis:    
+ * Authors:     MicroEmacs User
+ */
+
+const { Pargs } = require('./pargs.js');
+
+// Call the static method from the class
+console.log(Pargs.getMessage());
+
+console.log("Main TS program completed!");
